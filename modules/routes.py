913c9924f47f7882b4edc29ebbaf8e1555d5dee5
ChcCas <Rochnyak4@gmail.com>
2025-05-22 16:22:01 +0300

    Update routes.py

--- modules/routes.py
+++ modules/routes.py
@@ -14,10 +14,14 @@ def register_routes(app, application):
         else:
             abort(403)
 
- @app.route("/set-webhook", methods=["GET"])
-def set_webhook():
-    asyncio.run(application.bot.set_webhook(
-        url=WEBHOOK_URL,
-        drop_pending_updates=True
-    ))
-    return "✅ Webhook встановлено!"
+    @app.route("/set-webhook", methods=["GET"])
+    def set_webhook():
+        asyncio.run(application.bot.set_webhook(
+            url=WEBHOOK_URL,
+            drop_pending_updates=True
+        ))
+        return "✅ Webhook встановлено!"
+
+    @app.route("/")
+    def index():
+        return "Bot is alive!"
