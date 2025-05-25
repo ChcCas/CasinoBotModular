61b5809a6286498f91767fa72f8377e93911c050
ChcCas <Rochnyak4@gmail.com>
2025-05-25 11:34:12 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import os
 from telegram.ext import Application
-from modules.handlers import setup_handlers
+from modules.handlers import get_conv_handler  # ВАЖЛИВО! Імпортуємо get_conv_handler
+
 # 1) Змінні оточення (Render автоматично задає PORT)
 TOKEN       = os.environ["TOKEN"]
 PORT        = int(os.environ.get("PORT", "8443"))
@@ -10,8 +11,8 @@ def main():
     # 2) Створюємо екземпляр бота
     app = Application.builder().token(TOKEN).build()
 
-    # 3) Підключаємо всі handler-и з modules/handlers.py
-    setup_handlers(app)
+    # 3) Підключаємо ConversationHandler із modules/handlers.py
+    app.add_handler(get_conv_handler())  # Додаємо хендлер
 
     # 4) Запускаємо webhook-сервер + Telegram-клієнт в одному asyncio-циклі
     app.run_webhook(
