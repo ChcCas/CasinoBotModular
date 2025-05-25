ee47d0199b9190e2973d29ece0ec067210acd36b
ChcCas <Rochnyak4@gmail.com>
2025-05-25 11:46:17 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,20 +1,14 @@
 import os
 from telegram.ext import Application
-from modules.handlers import get_conv_handler  # ВАЖЛИВО! Імпортуємо get_conv_handler
+from modules.handlers import setup_handlers
 
-# 1) Змінні оточення (Render автоматично задає PORT)
 TOKEN       = os.environ["TOKEN"]
 PORT        = int(os.environ.get("PORT", "8443"))
 WEBHOOK_URL = os.environ["WEBHOOK_URL"]  # наприклад: https://casinobotmodular.onrender.com/webhook
 
 def main():
-    # 2) Створюємо екземпляр бота
     app = Application.builder().token(TOKEN).build()
-
-    # 3) Підключаємо ConversationHandler із modules/handlers.py
-    app.add_handler(get_conv_handler())  # Додаємо хендлер
-
-    # 4) Запускаємо webhook-сервер + Telegram-клієнт в одному asyncio-циклі
+    setup_handlers(app)
     app.run_webhook(
         listen="0.0.0.0",
         port=PORT,
